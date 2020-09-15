55f37b36daa78e98a3d16cf0567c4aad0556e6d3
Lukas Oertel <git@luoe.dev>
2020-09-15 10:25:21 +0200

    Build the cyberauftritt image before stopping container
    Thus reducing downtime.

--- homepage/update-cyberauftritt.sh
+++ homepage/update-cyberauftritt.sh
@@ -2,13 +2,18 @@
 
 cd /srv/docker/homepage/
 
+# build the container before restarting (this will reduce downtime)
+cd cyberauftritt-git
+git pull origin master
+cd ..
+docker-compose build
+
 # shut down the containers and remove them
 docker-compose down
 
 # pull new version of the code from github
 cd cyberauftritt-git
 rm -r _site
-git pull origin master
 mkdir _site
 chown 1000 -R _site
 
