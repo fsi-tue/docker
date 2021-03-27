76d1363b62d42dc6614c8c710efce051f036e1e7
Lukas Oertel <git@luoe.dev>
2021-03-27 12:27:02 +0100

    Add path for docker-compose
    Because we use the latest version from Github.
    Also use the --quiet option because it isn't run inside a terminal.

--- cyberauftritt/update-cyberauftritt.sh
+++ cyberauftritt/update-cyberauftritt.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+DOCKER_COMPOSE_PATH=/usr/local/bin
+
 cd /srv/docker/cyberauftritt
 
 # build the container before restarting (this will reduce downtime)
 cd cyberauftritt-git
 git pull origin master
 cd ..
-docker-compose build
+$DOCKER_COMPOSE_PATH/docker-compose build --quiet
 
 # shut down the containers and remove them
-docker-compose down
+$DOCKER_COMPOSE_PATH/docker-compose down
 
 # pull new version of the code from github
 cd cyberauftritt-git
@@ -18,6 +20,6 @@ mkdir _site
 chown 1000 -R _site
 
 cd ..
-docker-compose up -d --build
+$DOCKER_COMPOSE_PATH/docker-compose up -d --build --quiet
 
 exit 0
