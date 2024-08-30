416f6ed4a706062cac3d626fd70e12acc1e274e4
Lukas Oertel <git@luoe.dev>
2024-08-30 23:02:28 +0200

    Improve Nextcloud update script

--- _scripts/nextcloud_update-db-indizes.sh
+++ _scripts/nextcloud_update-db-indizes.sh
@@ -8,5 +8,11 @@ DOCKER_BINARY=/usr/bin/docker
 DIR=/srv/docker
 YML=docker-compose.yml
 
+$DOCKER_BINARY compose -f $DIR/nextcloud/$YML exec -u www-data app ./occ maintenance:mode --on
 $DOCKER_BINARY compose -f $DIR/nextcloud/$YML exec -u www-data app ./occ db:add-missing-indices
+$DOCKER_BINARY compose -f $DIR/nextcloud/$YML exec -u www-data app ./occ maintenance:repair --include-expensive
+$DOCKER_BINARY compose -f $DIR/nextcloud/$YML exec -u www-data app ./occ maintenance:mode --off
+$DOCKER_BINARY compose -f $DIR/kogni-nextcloud/$YML exec -u www-data app ./occ maintenance:mode --on
 $DOCKER_BINARY compose -f $DIR/kogni-nextcloud/$YML exec -u www-data app ./occ db:add-missing-indices
+$DOCKER_BINARY compose -f $DIR/kogni-nextcloud/$YML exec -u www-data app ./occ maintenance:repair --include-expensive
+$DOCKER_BINARY compose -f $DIR/kogni-nextcloud/$YML exec -u www-data app ./occ maintenance:mode --off
