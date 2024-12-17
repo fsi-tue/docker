195e5d9c27d338c293b32820df3c61b3f4ba8c8a
Lukas Oertel <git@luoe.dev>
2024-12-17 16:30:39 +0100

    pizzasystem: Init

---
+++ pizzasystem/update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Author: Lukas Oertel <git@luoe.dev>
+#
+# Script that is triggered by Github Actions that does the following things:
+# - Pulls the new code
+# - Builds the NodeJS app
+# - Force recreates the containers that new images are definitely used
+
+PIZZA_DATA=/srv/data/pizzasystem
+PIZZA_DOCKER=/srv/docker/pizzasystem
+
+/bin/chown 1001:1001 -R $PIZZA_DATA
+sudo -u 1001 /usr/bin/git -C $PIZZA_DATA pull origin main
+/usr/bin/docker compose -f $PIZZA_DOCKER/docker-compose.yml build --no-cache
+/usr/bin/docker compose -f $PIZZA_DOCKER/docker-compose.yml up -d --force-recreate
+# /bin/chown 1001:1001 -R $PIZZA_DATA
+
